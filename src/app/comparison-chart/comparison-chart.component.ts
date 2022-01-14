182a202f94cec6aa6ca84fd5df9fc78647e72ae4
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2022-01-14 15:23:05 -0600

    begin work on comparison chart

--- src/app/comparison-chart/comparison-chart.component.ts
+++ src/app/comparison-chart/comparison-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Stocks } from 'src/Interfaces';
+import { LineChart } from 'src/Interfaces';
 import { DataService } from '../data.service';
 import { StockService } from '../stock.service';
 
@@ -15,25 +15,71 @@ export class ComparisonChartComponent implements OnInit {
     private dataService:DataService
   ) { }
 
-  stocks:Stocks = <Stocks>{};
+  stocks:LineChart = <LineChart>{};
   
   ngOnInit(): void {
     this.stockService.getStockHistoricalData().subscribe((response)=>{
-      this.stocks = <Stocks>{};
+      this.stocks = <LineChart>{};
       this.stocks = {
         x:      this.unpackArray(response.data[0].data, "timestamp"),
-        close:  this.unpackArray(response.data[0].data, "close"),
-        high:   this.unpackArray(response.data[0].data, "high"),
-        low:    this.unpackArray(response.data[0].data, "low"),
-        open:   this.unpackArray(response.data[0].data, "open"),
+        y:  this.unpackArray(response.data[0].data, "close"),
         decreasing: {line: {color: '#7F7F7F'}}, 
         increasing: {line: {color: '#17BECF'}}, 
         line: {color: 'rgba(31,119,180,1)'}, 
-        type: 'candlestick', 
+        type: 'line', 
         xaxis: 'x', 
         yaxis: 'y' 
     }
     })
+  }
+
+  title = 'comparison-plots';
+  // Bar Chart
+  graph1 = {
+    data: [
+      {  
+        x: [''],
+        y: [''],
+    },
+    ],
+    layout: {
+      useResizeHandler: true,
+      autosize: true,
+      title: 'change',
+      margin: 0,
+      yaxis: {
+        autorange: true, 
+        fixedrange: false
+      },
+      xaxis: {
+        autorange: true, 
+        domain: [0, 1], 
+        color: 'pink',
+        title: 'Dates',
+        rangebreaks: [
+          {
+            bounds: ["sat", "mon"] 
+          },
+          {
+            bounds: [16, 9.5], 
+            pattern: "hour"
+          }
+        ]
+      }}
+  };
+
+
+  unpackArray(array:any[], key:string) {
+    return array.map(array => array[key]);
+  }
+
+  hover(event: any): void {
+    // this.interactivePlotSubject$.next(
+    //   [this.graph2.data[event.points[0].pointIndex]]
+    // );
+  }
+  mouseLeave(event:any): void {
+    // this.interactivePlotSubject$.next(this.graph2.data);
   }
 
 }
