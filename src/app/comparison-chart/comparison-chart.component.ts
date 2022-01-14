957d7af565f7eacd0c355bee00d4bd2c825607e8
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2022-01-14 09:48:50 -0600

    begin work on comparison

--- src/app/comparison-chart/comparison-chart.component.ts
+++ src/app/comparison-chart/comparison-chart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Stocks } from 'src/Interfaces';
+import { DataService } from '../data.service';
+import { StockService } from '../stock.service';
 
 @Component({
   selector: 'app-comparison-chart',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComparisonChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private stockService:StockService,
+    private dataService:DataService
+  ) { }
 
+  stocks:Stocks = <Stocks>{};
+  
   ngOnInit(): void {
+    this.stockService.getStockHistoricalData().subscribe((response)=>{
+      this.stocks = <Stocks>{};
+      this.stocks = {
+        x:      this.unpackArray(response.data[0].data, "timestamp"),
+        close:  this.unpackArray(response.data[0].data, "close"),
+        high:   this.unpackArray(response.data[0].data, "high"),
+        low:    this.unpackArray(response.data[0].data, "low"),
+        open:   this.unpackArray(response.data[0].data, "open"),
+        decreasing: {line: {color: '#7F7F7F'}}, 
+        increasing: {line: {color: '#17BECF'}}, 
+        line: {color: 'rgba(31,119,180,1)'}, 
+        type: 'candlestick', 
+        xaxis: 'x', 
+        yaxis: 'y' 
+    }
+    })
   }
 
 }
